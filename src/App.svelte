fb012fea781921b4e81594416ca1879f7a09abc1
Nirav Chavda <nmchavda99@gmail.com>
2022-02-14 23:42:15 +0530

    Basics of Svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,47 @@
   let firstName: string = "Nirav";
   let lastName: string = "Chavda";
   let color: string = "blue";
+  let showText: boolean = false;
+  let users = [
+    {
+      id: "1",
+      name: "Nirav",
+    },
+    {
+      id: "2",
+      name: "Ajinkya",
+    },
+    {
+      id: "3",
+      name: "Milind",
+    },
+  ];
 
   $: name = firstName + " " + lastName;
 
   const toggleColor = () => {
     color = color === "blue" ? "red" : "blue";
+    showText = !showText;
+    users = [...users, { id: "4", name: "Pradip" }];
   };
 </script>
 
 <main>
   <h1 style="color: {color}">Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  {#if showText}
+    <p>
+      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+      how to build Svelte apps.
+    </p>
+  {:else}
+    <p>No Text</p>
+  {/if}
 
   <button on:click={toggleColor}>Click</button>
+
+  {#each users as user (user.id)}
+    <h3>{user.id}: {user.name}</h3>
+  {/each}
 </main>
 
 <style>
